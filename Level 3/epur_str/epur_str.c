b41c69d5dc822cd15378fd89836191d8e65d8396
Mustafa YAĞIZ <112881823+myagjz@users.noreply.github.com>
2023-04-10 21:48:18 +0300

    Update epur_str.c

--- Level 3/epur_str/epur_str.c
+++ Level 3/epur_str/epur_str.c
@@ -1,9 +1,10 @@
 #include <unistd.h>
 
-int main(int ac, char **av)
+int		main(int ac, char **av)
 {
-	int i = 0;
+	int i;
 
+	i = 0;
 	if (ac == 2)
 	{
 		while (av[1][i] == ' ' || av[1][i] == '\t')
@@ -12,14 +13,14 @@ int main(int ac, char **av)
 		{
 			if (av[1][i] == ' ' || av[1][i] == '\t')
 			{
-				if (av[1][i + 1] > 32 && av[1][i + 1] == '\0')
-					write (1, " ", 1);
+				if (av[1][i + 1] > 32 && av[1][i + 1] != '\0')
+					write(1, " ", 1);
 			}
 			else if (av[1][i] != ' ' && av[1][i] != '\t')
-				write (1, &av[1][i], 1);
+				write(1, &av[1][i], 1);
 			i++;
 		}
 	}
-	write (1, "\n", 1);
+	write(1, "\n", 1);
 	return (0);
 }

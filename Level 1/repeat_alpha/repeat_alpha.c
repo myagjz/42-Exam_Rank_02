46573a3630cad6604b1a06d7915f7c4d4dfd2f02
Mustafa YAĞIZ <112881823+myagjz@users.noreply.github.com>
2023-04-10 23:29:45 +0300

    Update repeat_alpha.c

--- Level 1/repeat_alpha/repeat_alpha.c
+++ Level 1/repeat_alpha/repeat_alpha.c
@@ -1,27 +1,30 @@
 #include <unistd.h>
 
-int	main(int ac, char **av)
+int main(int ac, char **av)
 {
 	int i = 0;
 	int c = 0;
 
 	if (ac == 2)
 	{
-		while (av[1][i] != '\0')
+		while (av[1][i])
 		{
-			if (av[1][i] >= 'A' && av[1][i] <= 'Z')
-				c = av[1][i] - 64;
-			else if (av[1][i] >= 'a' && av[1][i] <= 'z')
+			if (av[1][i] >= 'a' && av[1][i] <= 'z')
 				c = av[1][i] - 96;
-			while (c)
+			else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
+				c = av[1][i] - 64;
+			if (c != 0)
 			{
-				write (1, &av[1][i], 1);
-				c--;
+				while (c > 0)
+				{
+					write(1, &av[1][i], 1);
+					c--;
+				}
 			}
-			c = 1;
+			else
+				write(1, &av[1][i], 1);
 			i++;
 		}
 	}
-	write (1, "\n", 1);
-	return (0);
+	write(1, "\n", 1);
 }

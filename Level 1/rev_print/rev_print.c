8be6be7f4e1ff97b0462483d1d759446e4a5c5f7
Mustafa YAĞIZ <112881823+myagjz@users.noreply.github.com>
2023-06-04 22:47:10 +0300

    Update rev_print.c

--- Level 1/rev_print/rev_print.c
+++ Level 1/rev_print/rev_print.c
@@ -1,18 +1,15 @@
-#include <unistd.h>
-
-int	main(int ac, char **av)
+char	*rev_print(char *str)
 {
 	int i = 0;
-
-	if (ac == 2)
+	
+	while (str[i])
+		i++;
+	i--;
+	
+	while (i >= 0)
 	{
-		while (av[1][i] != '\0')
-			i += 1;
-		while (i)
-		{
-			write (1, &av[1][--i], 1);
-		}
+		write (1, &str[i], 1);
+		i--;
 	}
-	write (1, "\n", 1);
-	return (0);
+	return (str);
 }
